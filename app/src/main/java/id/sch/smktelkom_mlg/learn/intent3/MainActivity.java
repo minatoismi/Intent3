014b7b345618d5866f11abd146fc341255eaeee8
minatoismi <ismi1ion4arspada@gmail.com>
2017-01-18 02:42:33 +0700

    Intent SMS

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -20,6 +20,14 @@ public class MainActivity extends AppCompatActivity {
                                                                  }
                                                              }
         );
+
+        findViewById(R.id.imageViewSMS).setOnClickListener(new View.OnClickListener() {
+                                                               @Override
+                                                               public void onClick(View v) {
+                                                                   composeSmsMessage("Pesan dari SMK Telkom Malang");
+                                                               }
+                                                           }
+        );
     }
 
     public void dialPhoneNumber(String phoneNumber) {
@@ -28,4 +36,10 @@ public class MainActivity extends AppCompatActivity {
         if (intent.resolveActivity(getPackageManager()) != null) startActivity(intent);
     }
 
+    public void composeSmsMessage(String message) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra("sms_body", message);
+        if (intent.resolveActivity(getPackageManager()) != null) startActivity(intent);
+    }
 }
